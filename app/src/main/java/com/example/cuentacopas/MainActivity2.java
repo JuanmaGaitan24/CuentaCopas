0aba18fa412f4f662df083d4fe9fd584738cb78e
JuanmaGaitan24 <juanmanuelgaitan@iesflorenciopintado.es>
2022-10-09 12:30:21 +0200

    Cuenta copas V4

--- app/src/main/java/com/example/cuentacopas/MainActivity2.java
+++ app/src/main/java/com/example/cuentacopas/MainActivity2.java
@@ -1,10 +1,18 @@
 package com.example.cuentacopas;
 
+import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Context;
 import android.content.Intent;
 import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
+import android.widget.Button;
+import android.widget.ScrollView;
 import android.widget.SeekBar;
 import android.widget.TextView;
 
@@ -15,6 +23,7 @@ public class MainActivity2 extends AppCompatActivity {
     TextView Ncopas;
     TextView TamText;
     SeekBar BarraTam;
+    ScrollView ListAmigos;
 
     @Override
     protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
@@ -29,6 +38,7 @@ public class MainActivity2 extends AppCompatActivity {
         Ncopas = findViewById(R.id.TextViewCantCopas);
         TamText = findViewById(R.id.textViewTamText);
         BarraTam = findViewById(R.id.seekBarTamanno);
+        ListAmigos = findViewById(R.id.ListaAmigos);
 
         Intent intent = getIntent();
 
@@ -38,7 +48,38 @@ public class MainActivity2 extends AppCompatActivity {
         Ncopas.setText(Ncopas.getText() + " " + copas);
         TamText.setText(TamText.getText() + " " + BarraTam.getProgress());
 
+        AdaptadorAmigos adaptadoramigos = new AdaptadorAmigos(this, R.layout.amigo, amigos);
+
 
 
     }
+
+    private class AdaptadorAmigos extends ArrayAdapter<String>{
+
+        public AdaptadorAmigos(@NonNull Context context, int resource, int amigos) {
+            super(context, resource);
+        }
+
+        @Override
+        public View getDropDownView(int position, @Nullable View convertView, @NonNull ViewGroup parent) {
+            return rellenarfila(position, convertView, parent);
+        }
+
+        private View rellenarfila(int position, View convertView, ViewGroup parent) {
+
+            LayoutInflater inflater = getLayoutInflater();
+            View miFila = inflater.inflate(R.layout.amigo,parent, false);
+
+            TextView Namigo = findViewById(R.id.textViewAmigo);
+            TextView cantcop = findViewById(R.id.textViewCantCop);
+            Button sumarcop = findViewById(R.id.buttonMas);
+            Button restarcop = findViewById(R.id.buttonMenos);
+
+            return miFila;
+
+        }
+
+
+    }
+
 }
